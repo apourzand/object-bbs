608e6ed89d58aa0b602c75ba4955cf5b26c26093
apourzand <info@modulotech.ch>
2021-07-19 14:04:11 +0200

    dx scheduler installed

---
+++ booking/src/App.js
@@ -0,0 +1,12 @@
+import Booking from './Components/Booking'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello fron booking</h1>
+      <Booking />
+    </div>
+  );
+}
+
+export default App;
---
+++ booking/src/Components/Booking.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import Paper from '@material-ui/core/Paper';
+import { alpha } from '@material-ui/core/styles';
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import {
+  Scheduler,
+  DayView,
+  Appointments,
+} from '@devexpress/dx-react-scheduler-material-ui';
+
+const currentDate = '2021-07-18';
+const schedulerData = [
+  { startDate: '2021-07-18T09:00', endDate: '2021-07-18T10:00', title: 'Meeting' },
+  { startDate: '2021-07-18T12:00', endDate: '2021-07-18T14:00', title: 'Go to a gym' },
+];
+
+export default () => (
+  <Paper>
+    <Scheduler
+      data={schedulerData}
+    >
+      <ViewState
+        currentDate={currentDate}
+      />
+      <DayView
+        startDayHour={9}
+        endDayHour={14}
+      />
+      <Appointments />
+    </Scheduler>
+  </Paper>
+);
