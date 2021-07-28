d3b999af94ce4fbf8cc40e9cb050d35653b3e2b1
apourzand <info@modulotech.ch>
2021-07-28 11:12:50 +0200

    Scheduler ressources demo

--- booking/src/Components/Booking/Booking.js
+++ booking/src/Components/Booking/Booking.js
@@ -1,31 +1,76 @@
-import * as React from 'react';
+import { useState } from 'react';
 import Paper from '@material-ui/core/Paper';
-import { ViewState } from '@devexpress/dx-react-scheduler';
+import { ViewState, EditingState } from '@devexpress/dx-react-scheduler';
 import {
   Scheduler,
-  DayView,
+  Resources,
+  MonthView,
   Appointments,
+  AppointmentTooltip,
+  AppointmentForm,
+  EditRecurrenceMenu,
+  DragDropProvider,
 } from '@devexpress/dx-react-scheduler-material-ui';
+import { owners } from './demo-data/tasks';
+import { appointments, resourcesData } from './demo-data/resources';
 
-const currentDate = '2021-07-18';
-const schedulerData = [
-  { startDate: '2021-07-18T09:00', endDate: '2021-07-18T10:00', title: 'Meeting' },
-  { startDate: '2021-07-18T12:00', endDate: '2021-07-18T14:00', title: 'Go to a gym' },
-];
+export default (props) => {
+  const [myData, setMyData] = useState({
+    data: appointments,
+    resources: [
+      {
+        fieldName: 'roomId',
+        title: 'Room',
+        instances: resourcesData,
+      },
+      {
+        fieldName: 'members',
+        title: 'Members',
+        instances: owners,
+        allowMultiple: true,
+      },
+    ],
+  });
 
-export default () => (
-  <Paper>
-    <Scheduler
-      data={schedulerData}
-    >
-      <ViewState
-        currentDate={currentDate}
-      />
-      <DayView
-        startDayHour={9}
-        endDayHour={14}
-      />
-      <Appointments />
-    </Scheduler>
-  </Paper>
-);
+  const commitChanges = ({ added, changed, deleted }) => {
+    let { data } = myData;
+
+    console.log('added: ' + JSON.stringify(added));
+    console.log('added: ' + JSON.stringify(changed));
+    console.log('added: ' + JSON.stringify(deleted));
+
+    return { data };
+  }
+
+  const { data, resources } = myData;
+
+  return (
+    <Paper>
+      <Scheduler
+        data={data}
+      >
+        <ViewState
+          defaultCurrentDate="2017-05-25"
+        />
+        <EditingState
+          onCommitChanges={commitChanges}
+        />
+        <EditRecurrenceMenu />
+
+        <MonthView />
+        <Appointments />
+        <AppointmentTooltip
+          showOpenButton
+        />
+        <AppointmentForm />
+
+        <Resources
+          data={resources}
+          mainResourceName="roomId"
+        />
+        <DragDropProvider />
+      </Scheduler>
+    </Paper>
+  );
+}
+
